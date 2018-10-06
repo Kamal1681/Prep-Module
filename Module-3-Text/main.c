e52dd993d7bb3c4b097d576899bab9696a9237f0
Kamal Maged <kamal_maged@yahoo.com>
2018-10-06 15:36:20 -0400

    Initial Commit

---
+++ Module-3-Text/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Module-3-Text
+//
+//  Created by Kamal Maged on 2018-10-06.
+//  Copyright © 2018 Kamal Maged. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
